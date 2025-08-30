c4b27fa94c224f88d3a20e384a8c02fd5a17b876
Anton Golubchikov <anton1programmist@gmail.com>
2025-08-30 19:37:57 +0700

    dotenv .env

--- src/task_tg_bot/__init__.py
+++ src/task_tg_bot/__init__.py
@@ -1,5 +1,16 @@
+import os
+
+import dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
+
+dotenv.load_dotenv(".env")
+
+TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -10,9 +21,12 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def run_bot() -> None:
-    app = ApplicationBuilder().token("TOKEN").build()  # TODO: add env file
+    global TELEGRAM_BOT_TOKEN
+    app = (
+        ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    )  # TODO: add env file
 
-    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("start", hello))
 
     print("BOT is running")
     app.run_polling()
